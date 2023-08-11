8d8c4a34100b31417be017ec33209068a19047ad
Tinu Ade <tinucy@yahoo.com>
2023-08-11 15:59:42 +0100

    added browser router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import viteLogo from "/vite.svg";
+// import viteLogo from "/vite.svg";
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
