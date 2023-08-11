eac24a19eb20ae52f343b8e6edd15722c3ff5a05
Tinu Ade <tinucy@yahoo.com>
2023-08-11 16:00:04 +0100

    added browser router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 // import viteLogo from "/vite.svg";
 import "./App.scss";
+//react-router-dom is a package that allows you to create navigation
+//in your app. It's a bit like React Router for
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
