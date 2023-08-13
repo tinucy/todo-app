522bfcfb3ebf668f162484f8c1157e42c49508b4
Tinu Ade <tinucy@yahoo.com>
2023-08-13 17:05:28 +0100

    changed the children in the Home fragment to start with upper case and the library imported also to start with cap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
+//all the names of the functions to import from the library must start with a capital letter
 import React from "react";
 import Container from "../components/container/Container";
 import Header from "../components/header/Header";
 import AddTodo from "../components/ass_todo/AddTodo";
-import list_todo from "../components/list_todo/list_todo";
-import footer from "../components/footer/footer";
+import List_todo from "../components/list_todo/list_todo";
+import Footer from "../components/footer/footer";
 
 export default function Home() {
   return (
+    // all the elements here must startwith Capital letter
     <Container>
       <Header />
       <AddTodo />
-      <list_todo />
-      <footer />
+      <List_todo />
+      <Footer />
     </Container>
   );
 }
